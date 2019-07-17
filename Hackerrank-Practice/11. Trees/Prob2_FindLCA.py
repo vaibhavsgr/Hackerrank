dc4a80c4cc81db6947f8c57e3c3dc64cdea7d689
vaibhav sagar <vaibhavsgr@gmail.com>
2019-07-17 14:42:50 +0530

    solution for LCA in BST

--- Hackerrank-Practice/11. Trees/Prob2_FindLCA.py
+++ Hackerrank-Practice/11. Trees/Prob2_FindLCA.py
@@ -47,18 +47,14 @@ class Node:
 '''
 
 def lca(root, v1, v2):
-    #Enter your code here
-    if self.root != None:
-        return _lca(root, v1, v2)
-    else: return 0
-
-def _lca(root, v1, v2):
-    # If both n1 and n2 are smaller than root, then LCA lies in left
-    if v1 < root && v2 < root:
-        return _lca(cur_node.left, v1, v2)
-
-    # If both n1 and n2 are greater than root, then LCA lies in right
-
+# If both n1 and n2 are greater than root, then LCA lies in right
+    while True:
+        if root.info>v1 and root.info>v2:
+            root = root.left
+        elif root.info<v1 and root.info<v2:
+            root = root.right
+        else:
+            return root
 
 
 tree = BinarySearchTree()
