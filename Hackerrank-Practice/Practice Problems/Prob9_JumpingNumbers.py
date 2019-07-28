47da0a14c13668a8aac316d9d2c6cb5a29d5aa79
vaibhav sagar <vaibhavsgr@gmail.com>
2019-07-28 18:21:30 +0530

    Working jumping nos

--- Hackerrank-Practice/Practice Problems/Prob9_JumpingNumbers.py
+++ Hackerrank-Practice/Practice Problems/Prob9_JumpingNumbers.py
@@ -1,6 +1,6 @@
 """
 A number is called as a Jumping Number if all adjacent digits in it differ by 1.
-The difference between ‘9’ and ‘0’ is not considered as 1.
+The difference between 9 and 0 is not considered as 1.
 All single digit numbers are considered as Jumping Numbers.
 For example 7, 8987 and 4343456 are Jumping numbers but 796 and 89098 are not
 
@@ -13,31 +13,46 @@ Output:  0 1 2 3 4 5 6 7 8 9 10 12
          67 76 78 87 89 98 101"""
 
 class Queue:
-    def __init__():
+    def __init__(self):
         self.q = []
 
     def enqueue(self, n):
         self.q.append(n)
 
-    def dequeue():
+    def dequeue(self):
         return self.q.pop(0)
 
     def is_empty(self):
-        return q=[]
+        return self.q==[]
 
 def breadthFirstSearch(n, limit):
     Q = Queue()
     Q.enqueue(n)
 
-    if n <= limit:
-        while(not Q.is_empty()):
-            num = Q.dequeue()
-            last_digit = num % 10
+    while(not Q.is_empty()):
+        n = Q.dequeue()
+        if n <= limit:
+            print (str(n) + " ")
+            last_digit = n % 10
 
             #if last_digit is 0, append last digit only
             if last_digit == 0:
-                Q.enqueue(num + (num/10)+1)
+                Q.enqueue(n*10 + last_digit+1)
 
             #if last_digit is 9, append first digit only
-            if last_digit == 9:
-                Q.enqueue(num + )
+            elif last_digit == 9:
+                Q.enqueue(n*10 + last_digit-1)
+
+            else:
+                Q.enqueue(n*10 + last_digit-1)
+                Q.enqueue(n*10 + last_digit+1)
+
+    #return Q
+
+if __name__ == "__main__":
+    print ("Enter the range")
+    limit = int(input())
+    #print (str(0), end =' ')
+    #n = 40
+    for n in range(10):
+        breadthFirstSearch(n, limit)
